6e035ece2f9c633ebc6e35c1fc36339d5fa36070
Hrithik Singh <96012516+curious04@users.noreply.github.com>
2024-09-17 12:25:33 +0530

    Update resume in personal-data.js

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -9,5 +9,5 @@ export const personalData = {
   github: 'https://github.com/curious04',
   linkedIn: 'https://www.linkedin.com/in/hrithik10/',
   twitter: 'https://twitter.com/hrithikcurious',
-  resume: "https://drive.google.com/file/d/1fm2bDn6DVWaR8pnYKJDdCvNfb1wmOlNS/view?usp=sharing"
+  resume: "https://drive.google.com/file/d/1fnfSf8BONsz3TW7crOZxGmRZkF-fMg8U/view?usp=sharing"
 }
